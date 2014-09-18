df96eaf1aacc5517ff119cc01d13868da1e03e76
Dam Peel <dampeel2000@gmail.com>
2014-09-18 23:15:35 +0200

    Test

--- grails-app/domain/albybail/Parametre.groovy
+++ grails-app/domain/albybail/Parametre.groovy
@@ -3,10 +3,12 @@ package albybail
 class Parametre {
 	
 	String	nom
+	String type
 	String	valeur
 
     static constraints = {
 		nom		blank: false, size: 3..50
+		type	blank: false, size: 3..50
 		valeur	blank: false, size: 3..200
     }
 }
